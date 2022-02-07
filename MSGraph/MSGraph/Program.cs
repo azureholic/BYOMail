58dd8c3f3f4bd3e50746b0f97a4e6638a79ccd7f
Remco Brosky <remcobrosky@microsoft.com>
2022-02-07 12:04:07 +0100

    inital commit

---
+++ MSGraph/MSGraph/Program.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Graph;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace MSGraph
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            SendMail().GetAwaiter().GetResult();
+            Console.WriteLine("Mail Send");
+            Console.ReadLine();
+        }
+
+        static private async Task SendMail()
+        {
+            var aadInstance = "https://login.microsoftonline.com/{0}";
+            var clientId = "<client id>";
+            var tenantId = "<aad tenant id>";
+            var clientSecret = "<client secret";
+            var fromAddress = "<mail from>";
+            var toAddress = "<mail to>";
+
+
+            ClientCredential cred = new ClientCredential(clientId, clientSecret);
+            AuthenticationContext authContext = new AuthenticationContext(String.Format(aadInstance, tenantId));
+
+            AuthenticationResult authResult = await authContext.AcquireTokenAsync("https://graph.microsoft.com/", cred);
+
+            var graphServiceClient = new GraphServiceClient(
+                new DelegateAuthenticationProvider((requestMessage) =>
+                {
+                    requestMessage
+                    .Headers
+                    .Authorization = new AuthenticationHeaderValue("bearer", authResult.AccessToken);
+
+                    return Task.CompletedTask;
+                }));
+
+
+            var message = new Message
+            {
+                Subject = "Can I send mail on behalf of" + fromAddress,
+                Body = new ItemBody
+                {
+                    ContentType = BodyType.Html,
+                    Content = "Yes you can!"
+                },
+                ToRecipients = new List<Recipient>()
+                {
+                    new Recipient
+                    {
+                        EmailAddress = new EmailAddress
+                        {
+                            Address = toAddress
+                        }
+                    }
+                }
+            };
+
+            
+            
+            await graphServiceClient.Users[fromAddress]
+                      .SendMail(message, true)
+                      .Request()
+                      .PostAsync();
+
+        }
+    }
+}
---
+++ SMTP/SMTP/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace SMTP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = "<smtp hostname>";
+            var port = 587; //SSL Port
+            var username = "<username auth smtp server>";
+            var password = "<password auth smtp server>";
+            var toAddress = "<mail to>";
+
+            SmtpClient cl = new SmtpClient();
+            cl.Host = host;
+            cl.Port = port;
+            cl.EnableSsl = true;
+
+            NetworkCredential cred = new NetworkCredential(username, password);
+            cl.Credentials = cred;
+
+            MailMessage msg = new MailMessage();
+            msg.Subject = "test";
+            msg.Body = "hello world";
+            msg.From = new MailAddress(username);
+            msg.To.Add(toAddress);
+            cl.Send(msg);
+            Console.WriteLine("Mail Send");
+            Console.ReadLine();
+
+        }
+    }
+}
